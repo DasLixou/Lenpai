c2042bdbf0f93c8d55cb8f3322546aac1b1ff217
DasLixou <einlixou@gmail.com>
2022-07-26 12:41:16 +0200

    More Styles

--- Lenpai.NodeSystem/NodeStyle.cs
+++ Lenpai.NodeSystem/NodeStyle.cs
@@ -3,15 +3,18 @@
     [Flags]
     public enum NodeStyle : byte
     {
-        Default =           0b00000000,
-        Statement =         0b00000001,
-        Block =             0b00000010,
-        BinaryOperator =    0b00000100,
-        UnaryOperator =     0b00001000,
+        Default =           0b10000000,
+        Statement =         0b10000001,
+        Block =             0b10000010,
+        BinaryOperator =    0b00000101,
+        UnaryOperator =     0b00000110,
 
-        Binary =            0b00010000,
-        Hex =               0b00100000,
+        Paren =             0b10001000,
 
-        Paren =             0b01000000,
+        Binary =            0b00010101,
+        Hex =               0b00010110,
+
+        SingleQuote =       0b00011001,
+        DoubleQuote =       0b00011010,
     }
 }
